57e46de52eaec0963cbbfc0a0beffc1badeb9ebc
marinlauber <M.Lauber@soton.ac.uk>
2020-11-19 18:18:49 +0100

    update _config.yml

--- docs/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "My First Post",
-        "excerpt":"Code Snippets   Print Hello, World! using python   print(\"Hello, World!\")   Equations   The Navier-Stokes Equations   \\[\\begin{split}    &amp;\\frac{\\partial \\vec{u}}{\\partial t} + \\left(\\vec{u}\\cdot\\nabla\\right)\\vec{u} = -\\frac{1}{\\rho}\\nabla p + \\nu\\nabla^2\\vec{u} + \\vec{f},\\\\    &amp;\\nabla\\cdot\\vec{u} = 0. \\end{split}\\]  Where $\\vec{u}$ is the velocity field and $p$ is the pressure field.  ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/HelloWorld/",
-        "teaser": null
-      }]

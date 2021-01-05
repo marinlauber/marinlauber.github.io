a5c2cda1be8806d798fb3d6eb8ff3643558cb314
marinlauber <M.Lauber@soton.ac.uk>
2021-01-05 18:47:38 +0100

    build with sitemap

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2,24 +2,24 @@ var store = [{
         "title": "My First Post",
         "excerpt":"Code Snippets   Print Hello, World! using python   print(\"Hello, World!\")   Equations   The Navier-Stokes Equations   \\[\\begin{split}    &amp;\\frac{\\partial \\vec{u}}{\\partial t} + \\left(\\vec{u}\\cdot\\nabla\\right)\\vec{u} = -\\frac{1}{\\rho}\\nabla p + \\nu\\nabla^2\\vec{u} + \\vec{f},\\\\    &amp;\\nabla\\cdot\\vec{u} = 0. \\end{split}\\]  Where $\\vec{u}$ is the velocity field and $p$ is the pressure field.  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/HelloWorld/",
+        "url": "https://marinlauber.github.io/HelloWorld/",
         "teaser": null
       },{
         "title": "Pseudo-spectral method for 2D Turbulence",
         "excerpt":"Mathematical Formualtion Vorticity equation The vorticity (or Helmholtz’s) equation describes the transport of vorticity. We can obtain it by applying the curl operator to the Navier-Stokes equations \\begin{equation} \\nabla \\times\\left( \\frac{\\partial\\boldsymbol{u}}{\\partial t} + (\\boldsymbol{u}\\cdot \\nabla)\\boldsymbol{u} = -\\frac{1}{\\rho}\\nabla p + \\nu \\nabla^2 \\boldsymbol{u}\\right), \\tag{1.1} \\end{equation} where $\\boldsymbol{u}$ is the velocity field...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/2D-Turbulence/",
+        "url": "https://marinlauber.github.io/2D-Turbulence/",
         "teaser": null
       },{
         "title": "APS-Division of Fluid Dynamics",
         "excerpt":"73rd Annual Meeting of the APS Division of Fluid Dynamics Pressure Treatment in Immersed Boundary Methods M.Lauber and G.D. Weymouth Maritime Engineering Group, University of Southampton 8.3 Computational Fluid Dynamics: Immersed Boundary Methods Abstract : Immersed boundary methods are extensively used for fluid-structure interaction problems involving large deformations and complex...","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/APS-DFD/",
+        "url": "https://marinlauber.github.io/APS-DFD/",
         "teaser": null
       },{
         "title": "Compact Schemes for the Poisson Equation",
         "excerpt":"This notebook demonstrates how to solve Poisson’s equation using Compact finite-difference schemes.                 ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/Compact-Poisson-Equation/",
+        "url": "https://marinlauber.github.io/Compact-Poisson-Equation/",
         "teaser": null
       }]
